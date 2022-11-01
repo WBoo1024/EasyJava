cfc49778cc6e697975d43535d5de8e80611ee2ac
壹零二四 <1533690878@qq.com>
2022-11-01 11:23:34 +0800

    文档部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,8 +5,6 @@ export default defineUserConfig({
     lang: "zh-CN",
     //网页标签及首页左上角logo
     title: "EasyJava",
-    //指定 vuepress build 的输出目录
-    dest: "./dist",
     theme,
     //是否开启默认预加载js
     shouldPrefetch: false,
