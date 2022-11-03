42eae743c78c2842338a4049acf13f0ca5359482
壹零二四 <1533690878@qq.com>
2022-11-03 23:45:43 +0800

    完善侧边栏功能

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,13 +3,12 @@ import {sidebar} from "vuepress-theme-hope";
 export default sidebar({
     //java部分
     "/java/": "structure",
-    "/java": [
-        {
-            text: "Java基础",
-            link: "/java/basis/01-基础概念.md",
-            icon: "java",
-        },
-    ],
+    //数据库部分
+    "/database/": "structure",
+    //spring部分
+    "/spring/": "structure",
+    //消息队列部分
+    "/mq/": "structure",
     //个人分享部分
     "/share/": "structure",
 });
