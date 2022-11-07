7acb08093c32b3f83345603c6380b1e6c387a8b9
壹零二四 <1533690878@qq.com>
2022-11-07 16:01:40 +0800

    添加百度统计脚本

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,4 +7,17 @@ export default defineUserConfig({
     title: "EasyJava",
     description: "今天你学习了吗?",
     theme,
+    //百度统计脚本
+    head: [
+        ["script",
+            {},
+            `var _hmt = _hmt || [];
+            (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?4602c358cd55c60e2d5781f736067523";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+            })();`,
+        ],
+    ],
 });
